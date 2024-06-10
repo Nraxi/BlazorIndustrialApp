986f635e25404a56ee04997b06d91bbe681c915e
Olle Tengnér <olle.code@hotmail.com>
2024-06-11 00:41:15 +0200

    Add project files.

---
+++ Models/car.cs
@@ -0,0 +1,16 @@
+﻿using System.Timers;
+
+namespace BlazorIndustrialApp.Models
+{
+    public class Car
+    {
+
+        public string ?Title { get; set; }
+        public string ?Model { get; set; }
+        public bool IsToggled { get; set; }
+        public bool IsStarted { get; set; }
+        public TimeSpan ElapsedTime { get; set; } = TimeSpan.Zero;
+        public System.Timers.Timer ?Timer { get; set; }
+
+    }
+}
